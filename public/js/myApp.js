f72b2070245a6b208bb78ea30cb9d1172328b436
Ian Bovard <thebestpantsintown@gmail.com>
2017-08-07 12:17:02 -1000

    step5 complete

---
+++ public/js/myApp.js
@@ -0,0 +1,12 @@
+/* jshint esversion:6*/
+angular.module('myApp', [/*list of dependencies*/]);
+let myApp = angular.module('myApp');
+
+myApp
+.config(function(){
+  //config
+})
+.run(function(){
+  //initialize
+  console.log('running');
+});
