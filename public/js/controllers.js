037b99c329161969a9606cf84bf2a1ebf3abc492
Ian Bovard <thebestpantsintown@gmail.com>
2017-08-07 14:48:22 -1000

    step11

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,9 @@
 /* jshint esversion:6*/
 angular.module('myApp')
-.controller('MyController', ['$scope', 'mainCharacter', function($scope, mainCharacter, APP_VERSION){
+.controller('MyController', ['$scope','CharacterVersionFactory', 'mainCharacter', function($scope, CharacterVersionFactory, mainCharacter){
   $scope.myFirstName = 'Cooking';
   $scope.myModel = 'Ready player one';
   $scope.character = mainCharacter;
+  $scope.cvfResult = CharacterVersionFactory.genCharVersion();
 
 }]);
