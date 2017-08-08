2b12a275f48b129fd95e79c7cfba82c1b46d22df
Ian Bovard <thebestpantsintown@gmail.com>
2017-08-07 15:05:18 -1000

    step12

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,10 @@
 /* jshint esversion:6*/
 angular.module('myApp')
-.controller('MyController', ['$scope','CharacterVersionFactory', 'mainCharacter', function($scope, CharacterVersionFactory, mainCharacter){
+.controller('MyController', ['$scope','CharacterVersionFactory', 'mainCharacter', 'BookService', function($scope, CharacterVersionFactory, mainCharacter, BookService){
   $scope.myFirstName = 'Cooking';
   $scope.myModel = 'Ready player one';
   $scope.character = mainCharacter;
   $scope.cvfResult = CharacterVersionFactory.genCharVersion();
+  $scope.books = BookService.getBooks();
 
 }]);
